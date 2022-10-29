66bdf240cce75f9e52fea7d4ad8c0c5fc542ff11
Seb <sebcyde@hotmail.com>
2022-10-30 00:22:37 +0100

    working poke population no dupes, no memory overloads

--- src/API/Pokemon.ts
+++ src/API/Pokemon.ts
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useState } from 'react';
 
@@ -12,27 +13,58 @@ interface PokemonCounter {
 }
 
 export const PopulatePokemon = async () => {
+	let Pokes: any[] = [];
 	let TotalPokemon = {
 		Pokemon: 1154,
 		Count: 0,
 	};
-	let Pokes: any[] = [];
 
-	let InitialCall = await axios.get(
+	const InitialCall = await axios.get(
 		'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0'
 	);
-	console.log(InitialCall.data);
+	console.log('Initial Call Length:', InitialCall.data.results.length);
 
-	// while (TotalPokemon.Count !== 1154) {
-	// const AllPokemon = await InitialCall.data.results.forEach(
-	// 	(Pokemon: InitialPokemon) => {
-	// 		axios.get(Pokemon.url).then((result) => {
-	// 			let Poke = result.data;
-	// 			Pokes.push(Pokes.filter((Pokes) => !Pokes.includes(Poke)));
-	// 			TotalPokemon = { ...TotalPokemon, Count: TotalPokemon.Count + 1 };
-	// 		});
-	// 	}
-	// );
+	const PokePopper = async () => {
+		let A = 0;
+		let B = 300;
+		let C = 1;
 
-	console.log(Pokes);
+		const DupeChecker = () => {
+			console.log('Running Dupe Checker');
+			Pokes.forEach((Poke, index) => {
+				Pokes.forEach((Poke2, index2) => {
+					if (Poke.name === Poke2.name && index !== index2) {
+						console.log('DUPE!');
+						console.log('Poke Name:', Poke.name);
+						console.log('Poke2 Name:', Poke2.name);
+					}
+				});
+			});
+		};
+
+		let Populate = () => {
+			Pokes.length === 1154
+				? clearInterval(Pop)
+				: console.log('Populating Iteration:', C);
+			while (A <= B - 1) {
+				if (InitialCall.data.results[A] === undefined) {
+					break;
+				}
+				axios
+					.get(InitialCall.data.results[A].url)
+					.then((result) => Pokes.push(result.data));
+				A++;
+			}
+			DupeChecker();
+			B = B + 300;
+			C = C + 1;
+		};
+
+		let Pop = setInterval(Populate, 3000);
+		setTimeout(() => console.log(Pokes), 12000);
+	};
+
+	PokePopper();
+
+	return { TotalPokemon };
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import { PopulatePokemon } from './API/Pokemon';
 
 function App() {
+	const [Renders, setRenders] = useState<number>(0);
+	const [Returned, setReturned] = useState<any>();
+	const [Loading, setLoading] = useState<boolean>(true);
+
+	const Initialise = async () => {
+		const Init = await PopulatePokemon();
+		setReturned(Init);
+		console.log(Returned);
+		setRenders(Renders + 1);
+		setLoading(false);
+	};
+
 	useEffect(() => {
-		PopulatePokemon();
+		Initialise();
 	}, []);
 
-	return <div className="App"></div>;
+	return (
+		<div
+			className="App"
+			style={{
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}
+		>
+			{/* {Return}
+			<h2 style={{ color: 'red' }}>{Return}</h2>
+			<h2 style={{ color: 'red' }}>
+				Total Pokemon: {Return.RawReturnedTotalPokemon.Pokemon}
+			</h2> */}
+			{Loading ? (
+				<h2 style={{ color: 'red' }}>Loading...</h2>
+			) : (
+				<h2 style={{ color: 'red' }}>Count: {Returned.TotalPokemon.Count}</h2>
+			)}
+		</div>
+	);
 }
 
 export default App;
